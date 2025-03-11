f95fc880cf396d3106430aa71a23d57af7f490ec
kiwihodl <orangeamber@protonmail.com>
2025-03-11 09:16:23 -0500

    fixing the open graph image 3

--- src/components/opengraph-image.tsx
+++ src/components/opengraph-image.tsx
@@ -1,5 +1,4 @@
 import { ImageResponse } from "next/og";
-import LogoIcon from "./icons/logo";
 
 export type Props = {
   title?: string;
@@ -10,16 +9,29 @@ export default async function OpengraphImage(
 ): Promise<ImageResponse> {
   const { title } = {
     ...{
-      title: "Brand Logo",
+      title: process.env.NEXT_PUBLIC_SITE_NAME,
     },
     ...props,
   };
+
+  // Load the logo image directly
+  const logoImageData = await fetch(
+    new URL("../../public/SULogo.png", import.meta.url)
+  ).then((res) => res.arrayBuffer());
 
   return new ImageResponse(
     (
       <div tw="flex h-full w-full flex-col items-center justify-center bg-black">
         <div tw="flex flex-none items-center justify-center border border-neutral-700 h-[160px] w-[160px] rounded-3xl">
-          <LogoIcon width="64" height="58" fill={true} />
+          <img
+            src={logoImageData as any}
+            alt="Sovereign University Logo"
+            width="64"
+            height="58"
+            style={{
+              objectFit: "contain",
+            }}
+          />
         </div>
         <p tw="mt-12 text-6xl font-bold text-white">{title}</p>
       </div>
