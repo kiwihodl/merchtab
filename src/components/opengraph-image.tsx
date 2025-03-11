12a303f7ea50374dc87d95367cf1bb4edc6801f7
kiwihodl <orangeamber@protonmail.com>
2025-03-11 09:19:43 -0500

    fixing the open graph image 4 - svg

--- src/components/opengraph-image.tsx
+++ src/components/opengraph-image.tsx
@@ -14,24 +14,14 @@ export default async function OpengraphImage(
     ...props,
   };
 
-  // Load the logo image directly
-  const logoImageData = await fetch(
-    new URL("../../public/SULogo.png", import.meta.url)
-  ).then((res) => res.arrayBuffer());
-
   return new ImageResponse(
     (
       <div tw="flex h-full w-full flex-col items-center justify-center bg-black">
         <div tw="flex flex-none items-center justify-center border border-neutral-700 h-[160px] w-[160px] rounded-3xl">
-          <img
-            src={logoImageData as any}
-            alt="Sovereign University Logo"
-            width="64"
-            height="58"
-            style={{
-              objectFit: "contain",
-            }}
-          />
+          <svg width="64" height="58" viewBox="0 0 60 60" fill="white">
+            <path d="M30 0C13.4315 0 0 13.4315 0 30C0 46.5685 13.4315 60 30 60C46.5685 60 60 46.5685 60 30C60 13.4315 46.5685 0 30 0ZM30 10C41.0457 10 50 18.9543 50 30C50 41.0457 41.0457 50 30 50C18.9543 50 10 41.0457 10 30C10 18.9543 18.9543 10 30 10Z" />
+            <path d="M30 15C21.7157 15 15 21.7157 15 30C15 38.2843 21.7157 45 30 45C38.2843 45 45 38.2843 45 30C45 21.7157 38.2843 15 30 15ZM30 25C35.5228 25 40 29.4772 40 35C40 40.5228 35.5228 45 30 45C24.4772 45 20 40.5228 20 35C20 29.4772 24.4772 25 30 25Z" />
+          </svg>
         </div>
         <p tw="mt-12 text-6xl font-bold text-white">{title}</p>
       </div>
