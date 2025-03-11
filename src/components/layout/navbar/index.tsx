7683eaa9d36da5db02e7179129b454a25cd39093
kiwihodl <orangeamber@protonmail.com>
2025-03-11 08:46:56 -0500

    testing mobile nav spacing

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -11,13 +11,13 @@ export async function Navbar() {
   return (
     <nav className="flex items-center justify-between p-4 lg:px-6 sticky top-0 backdrop-blur-sm bg-black/70 z-[999]">
       <div className="flex w-full items-center justify-between relative">
-        <div className="flex z-10 items-center gap-4">
+        <div className="flex z-10 items-center gap-1 md:gap-4">
           <Link href="/" prefetch={true} className="flex items-center">
             <LogoSquare />
           </Link>
           <MobileMenu />
         </div>
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 lg:static lg:left-auto lg:top-auto lg:transform-none">
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[180px] md:max-w-[300px] lg:static lg:left-auto lg:top-auto lg:transform-none">
           <Search />
         </div>
         <div className="flex items-center z-10">
